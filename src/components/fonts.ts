4dc73d46cb229b8c44286bb8375dfd6086593adc
nguyentrongbut <cloly031128@gmail.com>
2025-05-13 12:28:12 +0700

    chore: configure 'manarope' as default and 'roboto' secondary

---
+++ src/components/fonts.ts
@@ -0,0 +1,11 @@
+import {Manrope, Roboto} from "next/font/google";
+
+const manrope = Manrope({subsets: ['latin'], variable: "--font-manrope"});
+
+const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['400', '500', '700'],
+    variable: "--font-roboto"
+});
+
+export {manrope, roboto};
