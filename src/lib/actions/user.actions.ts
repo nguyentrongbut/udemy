6af8da78ff70d26d17a4ea82447b97b129fbef59
nguyentrongbut <cloly031128@gmail.com>
2025-05-15 13:37:53 +0700

    test

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,14 +1,15 @@
-"use server"
-import {connectToDatabase} from "@/lib/mongoose";
-import User from "@/database/user.model";
-import {TCreateUserParams} from "@/types";
+"use server";
 
-export default async function createUse(params: TCreateUserParams) {
+import User from "@/database/user.model";
+import { TCreateUserParams } from "@/types";
+import { connectToDatabase } from "../mongoose";
+
+export default async function createUser(params: TCreateUserParams) {
     try {
         connectToDatabase();
         const newUser = await User.create(params);
         return newUser;
-    }catch (error) {
-        console.log("error", error)
+    } catch (error) {
+        console.log(error);
     }
 }
