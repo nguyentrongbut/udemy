862767ca7145e2e633a984a990aad48930c0a677
nguyentrongbut <cloly031128@gmail.com>
2025-05-15 13:38:38 +0700

    fix(ts)

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -27,6 +27,7 @@ export async function POST(req: Request) {
             "svix-signature": svix_signature,
         }) as WebhookEvent;
     } catch (err) {
+        console.log(err)
         return new Response("Bad Request", { status: 400 });
     }
 
