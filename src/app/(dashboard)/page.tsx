76fa4c90f296a3cc2c19145acb8bf4b2e790c950
nguyentrongbut <cloly031128@gmail.com>
2025-05-15 13:12:45 +0700

    chore: delete create test database use in home

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,8 @@
 import Heading from "@/components/typography/Heading";
 import CourseItem from "@/components/course/CourseItem";
 import {CourseGrid} from "@/components/common";
-import createUse from "@/lib/actions/user.actions";
 
-export default async function Home () {
-    const user = await createUse({
-        clerkId: "clerk123",
-        email_address: "cloly@gmail.com",
-        username: "cloly",
-    })
-    console.log(user)
+export default function Home () {
   return (
     <>
       <Heading>
